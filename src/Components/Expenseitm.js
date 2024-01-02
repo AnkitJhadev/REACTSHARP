dfef5361a4031ecaad7cb586f5d5e3fdd916af80
AnkitJhadev <jha80250@gmail.com>
2024-01-02 16:00:53 +0530

    Added Component ExpenseItems to it

---
+++ src/Components/Expenseitm.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return(
+     <div>
+        <h2>Expense Items</h2>
+        <h3>FOOD RUPEES 50</h3>
+        <h3>PETROL RUPEES 150</h3>
+        <h3>MOVIES RUPEES 200</h3>
+
+
+
+     </div>
+    );
+}
+
+export default ExpenseItem;
