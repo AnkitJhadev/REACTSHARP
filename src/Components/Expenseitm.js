ca2a8266dc6980da27e7a567c0174acb3fa02699
AnkitJhadev <jha80250@gmail.com>
2024-01-02 18:22:16 +0530

    Adding variable to our Expenseitem file

--- src/Components/Expenseitm.js
+++ src/Components/Expenseitm.js
@@ -1,14 +1,18 @@
+import './Expenseitem.css'
 function ExpenseItem() {
+    const expensedate = new Date(2001,3,20)
+    const expenseTitle = 'CAR INSURANCE'
+    const expenseAmount = 295
+    const locationofexpenditure = 'DELHI'
     return(
-     <div>
-        <h2>Expense Items</h2>
-        <h3>FOOD RUPEES 50</h3>
-        <h3>PETROL RUPEES 150</h3>
-        <h3>MOVIES RUPEES 200</h3>
-
-
-
-     </div>
+     <div className='expense-item'>
+      <div>{expensedate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle
+        }  {   locationofexpenditure}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
     );
 }
 
