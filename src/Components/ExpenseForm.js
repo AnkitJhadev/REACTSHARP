7dd861d8ab669b628bfc1273789e128fe1f9e478
AnkitJhadev <jha80250@gmail.com>
2024-01-06 19:21:11 +0530

    Added form handler to git

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,13 +1,41 @@
 import "./ExpenseForm.css";
-
+import React,{useState} from "react";
 function ExpenseForm() {
-    const titleHandler = (event) =>{
-            console.log(event.target.value)
-    }
-    const handleSu = (event) =>{
-        console.log(event.target.value)
 
+  const [enteredTitle,setEnteredTitle]=useState('')
+    const titleHandler = (event) =>{
+            setEnteredTitle(event.target.value);
     }
+
+    const [enteredAmount,setEnteredAmount] = useState('');
+
+    const amountHandler = (event) =>{
+         setEnteredAmount(event.target.value)
+    }
+
+    const [enteredDate,setenteredDate] = useState('');
+
+    const dateHandler = (event) =>{
+        setenteredDate(event.target.value);
+    }
+  
+    const SubmitEvent = () => {
+      // Accessing the values from the component's state
+      const submittedTitle = enteredTitle;
+      const submittedAmount = enteredAmount;
+      const submittedDate = new Date(enteredDate);
+  
+      // Creating an object with the extracted values
+      const formData = {
+          title: submittedTitle,
+          amount: submittedAmount,
+          date: submittedDate
+      };
+  
+      // Logging the object containing the form data
+      console.log(formData);
+  }
+  
   return (
     <div className="new-expense__controls">
       <div className="new-expense__control">
@@ -16,15 +44,15 @@ function ExpenseForm() {
       </div>
       <div className="new-expense__control">
         <label>AMOUNT</label>
-        <input type="number" />
+        <input type="number" onChange={amountHandler}/>
       </div>
       <div className="new-expense__control">
         <label>DATE</label>
-        <input type="text" />
+        <input type="text" onChange={dateHandler}/>
       </div>
       <div className="new-expense__actions">
 
-        <button onClick={handleSu}>SUBMIT</button>
+        <button onClick={SubmitEvent}>SUBMIT</button>
       </div>
     </div>
   );
